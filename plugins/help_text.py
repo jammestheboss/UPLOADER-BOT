6e01ecd9555043d738b3d066e5c38ad57537a14d
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-11 06:28:31 -0600

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -61,3 +61,15 @@ async def start(bot, update):
         ),
         reply_to_message_id=update.message_id
     )
+    
+@Clinton.on_message(filters.private & filters.command(["about"]))
+def about(bot, update):
+    
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True   
+    ) 
+        
