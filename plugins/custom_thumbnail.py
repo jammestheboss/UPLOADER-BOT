d0ed93f745df5a8c3d50b65cf9168a71788a514e
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-11 04:28:28 -0600

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -35,13 +35,13 @@ async def save_photo(bot, update):
     await clinton.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
     await bot.send_message(chat_id=update.chat.id, text=Translation.SAVED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
 
-@Clinton.on_message(filters.private & filters.command("delthumbnail"))
+@Clinton.on_message(filters.private & filters.command("delthumb"))
 async def delthumbnail(bot, update):
     await AddUser(bot, update)
     await clinton.set_thumbnail(update.from_user.id, thumbnail=None)
     await bot.send_message(chat_id=update.chat.id, text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
 
-@Clinton.on_message(filters.private & filters.command("viewthumbnail") )
+@Clinton.on_message(filters.private & filters.command("showthumb") )
 async def viewthumbnail(bot, update):
     await AddUser(bot, update)
     thumbnail = await clinton.get_thumbnail(update.from_user.id)
