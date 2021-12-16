b8e6d2a4f8f8c60358c21f31934d5ab74348544d
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-16 09:17:58 -0600

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -96,6 +96,12 @@ async def echo(bot, update):
     if youtube_dl_password is not None:
         command_to_exec.append("--password")
         command_to_exec.append(youtube_dl_password)
+    if "hotstar" in url:
+        command_to_exec.append("--geo-bypass-country")
+        command_to_exec.append("IN")
+    if "france" in url:
+        command_to_exec.append("--geo-bypass-country")
+        command_to_exec.append("FR")
     # logger.info(command_to_exec)
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
